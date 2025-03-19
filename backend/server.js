6fd2d1a316d8ad2c74471c81783e20fa48647934
prachi1025 <prachipargal123@gmail.com>
2025-03-20 03:56:35 +0530

    make productSchema and api to create new product

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,35 @@
 import express from "express"
 import dotenv from "dotenv"
+import bodyParser from "body-parser"
 import { connectDB } from "./config/db.js"
+import { Product } from "./models/product.model.js"
 
 dotenv.config()
 
 const app = express()
 const port = 3000
 
-app.get("/products", (req, res) => {
+// app.use(express.json()) allows us to accept data in JSON in the req.body
 
+app.use(bodyParser.urlencoded({ extended:true }))
+//allows us to accept data in url form encoded in req.body
+
+app.post("/products", async (req, res) => {
+    const product = req.body
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success:false, message: "Please provide all fields" })
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+        res.status(201).json({ success:true, data:newProduct })
+    } catch(error) {
+        console.log("error while creating new product: ", error.message)
+        res.status(500).json({ success:false, message:"server error"})
+    }
 })
 
 console.log(process.env.MONGO_URI)
@@ -18,6 +39,6 @@ app.listen(port, () => {
         connectDB()
         console.log(`server started at ${port}`)
     } catch(error) {
-        console.log("failed to start the server :", error)
+        console.log("failed to start the server :", error.message)
     }
 })
